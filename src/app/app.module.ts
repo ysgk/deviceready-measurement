b87771524b3e338d68bbbc3b89cffa5e29327575
ysgk <yasugi.kohei@gmail.com>
2016-12-13 17:35:04 +0900

    ionicに対応

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { IonicApp, IonicModule } from 'ionic-angular';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    IonicModule.forRoot(AppComponent)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ IonicApp ]
 })
 export class AppModule { }
